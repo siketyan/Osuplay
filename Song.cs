7de723e03233e9e5071ac2a9dc9b4417699c7457
Siketyan <siketyan@sikeserver.com>
2017-06-08 17:28:38 +0900

    起動時にStackOverflowExceptionが発生する問題を修正

--- Song.cs
+++ Song.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Windows;
 
 namespace osu_Player
 {
@@ -29,33 +30,38 @@ namespace osu_Player
                 id = long.Parse(new Regex(@"^[0-9]+").Match(folder.Name).Groups[0].ToString());
             }
 
+            var properties = new Dictionary<string, string>();
             var stream = new StreamReader(osuFile[0].FullName);
             while (!stream.EndOfStream)
             {
                 var line = stream.ReadLine();
                 if (line == null) continue;
 
-                var properties = new Dictionary<string, string>();
-
                 if (line.Contains(":"))
                 {
                     var splitted = line.Split(':');
                     properties.Add(splitted[0], splitted[1]);
                 }
-
-                Title = properties.ContainsKey("TitleUnicode") ? properties["TitleUnicode"] : properties["Title"];
-                Artist = properties.ContainsKey("ArtistUnicode") ? properties["ArtistUnicode"] : properties["Artist"];
-                AudioPath = folder.FullName + @"\" + properties["AudioFilename"];
             }
-            
+
+            var hoge = "";
+            foreach (var property in properties)
+            {
+                hoge += property.Key + "::" + property.Value + "\n";
+            }
+
+            Title = properties.ContainsKey("TitleUnicode") ? properties["TitleUnicode"] : properties["Title"];
+            Artist = properties.ContainsKey("ArtistUnicode") ? properties["ArtistUnicode"] : properties["Artist"];
+            AudioPath = folder.FullName + @"\" + properties["AudioFilename"];
             ThumbnailPath = folder.FullName + @"\..\..\Data\bt\" + id + ".jpg";
+
             if (!File.Exists(ThumbnailPath))
             {
                 ThumbnailPath = "Resources/unknown.png";
             }
 
             Tag = Title + "\t" + Artist + "\t" + AudioPath + "\t" + ThumbnailPath;
-            stream.Dispose();            
+            stream.Dispose();
         }
 
         public Song(string tag)
